42083a8f77d7e26336067f3f83e3565dbf545962
Arunava Nag <arunavoster@gmail.com>
2023-05-12 18:35:23 -0700

    removed infinite while loop

--- BERTGenerator.py
+++ BERTGenerator.py
@@ -8,8 +8,6 @@ import torch
 from transformers import DistilBertTokenizer, DistilBertForMaskedLM
 
 
-
-
 # Load the pre-trained DistilBERT model and tokenizer
 model_name = "distilbert-base-uncased"
 tokenizer = DistilBertTokenizer.from_pretrained(model_name)
@@ -35,11 +33,11 @@ if torch.cuda.is_available():
 # Load and preprocess data
 dir = '~/DataAnalysis/data/Sprints/HighRes/'
 df = pd.read_hdf(dir+'Windy/WindyMASigned.h5')
-df1=df[0:50000]
+df1=df[15000:18000]
 df1=df1.round(3)
 
 # Test Dataset
-test=df[15000:20000]
+test=df[15000:15100]
 test=test.round(3)
 
 
@@ -59,19 +57,37 @@ for chunk_index in range(num_chunks):
     for idx, row in chunk_df.iterrows():
         text_data += f"Odor encounter: {row['odor']}, Location: ({row['xsrc']}, {row['ysrc']}), U velocity: {row['U']}, V velocity: {row['V']}.\n"
 
+print('Done Chunking process')
 
 
 
+
+def extract_values(generated_text):
+    pattern = r"odor encounter\s*:\s*([\d.]*)\s*([\d.]*)\s*,\s*location\s*:\s*\(\s*([\d.]*)\s*([\d.]*)\s*,\s*([\d.]*)\s*([\d.]*)\s*\)"
+    match = re.search(pattern, generated_text)
+    
+    if match:
+        odor = float(match.group(1) + match.group(2))
+        xsrc = float(match.group(3) + match.group(4))
+        ysrc = float(match.group(5) + match.group(6))
+        
+
+        return {"odor": odor, "xsrc": xsrc, "ysrc": ysrc}
+    else:
+        return None
 
 def generate_odor_with_bert(input_row):
     prompt = f"{text_data}\nGenerate a new odor encounter with the following location: ({input_row['xsrc']}, {input_row['ysrc']}) and U velocity: {input_row['U']}, V velocity: {input_row['V']}.Please use the format: Odor encounter: X, Location: (X, Y)"
-
-    pattern = r"Odor encounter: ([+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?), Location: \(([+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?), ([+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?)\)"
+    
+    # print(prompt)
     match = None
-
-    while not match:
+    max_iterations = 50
+    iteration = 0
+    
+    while not match and iteration < max_iterations:
+        
         # Tokenize the input prompt
-        input_ids = tokenizer.encode(prompt, return_tensors='pt',max_length=512, truncation=True)
+        input_ids = tokenizer.encode(prompt, return_tensors='pt', truncation=True)
      
         mask_idx = len(input_ids[0]) - 1
 
@@ -94,18 +110,17 @@ def generate_odor_with_bert(input_row):
 
         # Generate text for each predicted token
         for index in top_k_indices:
-            generated_text = tokenizer.decode(input_ids[0].tolist()[:-1] + [index.item()])
-            match = re.search(pattern, generated_text)
+            generated_text = tokenizer.decode(input_ids[0].tolist()[:-1] + [index.item()]).strip()
+            match = extract_values(generated_text)
+        
             if match:
                 break
-
+                
+        iteration +=1
+    
     if match:
-        new_entry = {
-            "odor": float(match.group(1).strip()),
-            "xsrc": float(match.group(2).strip()),
-            "ysrc": float(match.group(3).strip()),
-        }
-        return new_entry
+
+        return match
     else:
         print("Failed to parse generated text")
         print("Generated text:", generated_text)
@@ -113,18 +128,20 @@ def generate_odor_with_bert(input_row):
 
 
 def main():
- 
+
     # Create a new DataFrame to store generated data
     generated_df = pd.DataFrame(columns=['odor', 'xsrc', 'ysrc'])
-    num_threads = 8 
-
+    num_threads = 14 
+    print('Generating Data')
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
         results = list(executor.map(generate_odor_with_bert, test.to_dict('records')))
-
+ 
+    print('Done Generating Data')
     for result in results:
         if result is not None:
             generated_df = pd.concat([generated_df, pd.DataFrame([result])], ignore_index=True)
 
+    generated_df = generated_df.astype(float)
     generated_df.to_hdf('../generated.h5',key='generated_df',mode='w')
 
 
